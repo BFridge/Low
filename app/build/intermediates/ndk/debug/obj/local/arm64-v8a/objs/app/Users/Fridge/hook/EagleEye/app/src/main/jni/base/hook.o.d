ef96a3fba45df3b91d667fc768938ab46d5727d4
DONG Zesong <dongzesong_sx@qiyi.com>
2016-04-23 11:53:23 +0800

    Initial commit

---
+++ app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/app/Users/Fridge/hook/EagleEye/app/src/main/jni/base/hook.o.d
@@ -0,0 +1,8 @@
+/Users/Fridge/hook/EagleEye/app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/app//Users/Fridge/hook/EagleEye/app/src/main/jni/base/hook.o: \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/base/hook.c \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/base/util.h \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/base/hook.h
+
+/Users/Fridge/hook/EagleEye/app/src/main/jni/base/util.h:
+
+/Users/Fridge/hook/EagleEye/app/src/main/jni/base/hook.h:
---
+++ app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/app/Users/Fridge/hook/EagleEye/app/src/main/jni/entry.o.d
@@ -0,0 +1,11 @@
+/Users/Fridge/hook/EagleEye/app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/app//Users/Fridge/hook/EagleEye/app/src/main/jni/entry.o: \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/entry.c \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/hooks/util.h \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/hooks/../base/hook.h \
+ /Users/Fridge/hook/EagleEye/app/src/main/jni/base/util.h
+
+/Users/Fridge/hook/EagleEye/app/src/main/jni/hooks/util.h:
+
+/Users/Fridge/hook/EagleEye/app/src/main/jni/hooks/../base/hook.h:
+
+/Users/Fridge/hook/EagleEye/app/src/main/jni/base/util.h:
